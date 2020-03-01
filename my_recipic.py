67587db491347c2a0d59b505fa2997434a0353a2
조원행 <god2ida@gmail.com>
2020-03-01 16:20:50 +0900

    add_html_file
    html파일 백종원과 김수미 합침

--- my_recipic.py
+++ my_recipic.py
@@ -36,8 +36,6 @@ def follow_recipes_view():
 # Developer 조원행
 # 김수미 공식레시피 API
 @app.route('/s_official_recipes', methods=['GET'])
-def official_recipes_view():
-
 def s_official_recipes_view():
     # 서버 내부에서 수행 할 기능 / DB에 저장돼있는 모든 정보 중 '공식레시피' 가져오기
     soomis_official = list(db.soomi_all_recipes.find({'category':'공식레시피'}, {'_id': 0}))
@@ -258,15 +256,15 @@ def s_myrecipes_follow_view():
 ##################################### 레시피 랜덤 뿌리기 API #####################################
 #Developer 조원행
 #김수미 레시피 title 랜덤으로 뿌리기
-@app.route('/paik_rand_follow_recipes', methods=['GET'])
+@app.route('/paik_rand_official_recipes', methods=['GET'])
 def random_recipes():
     # paik_all_recipes 10번 돌면서 recipe 축적
     for repeat in range(10):
         # 백종원의 공식 레시피 400개에서 title만 꺼내온다
         random_recipe = list(db.paik_all_recipes.find({'category':'공식레시피'}, {'_id': 0}))
-        paik_random_recipe = (random.sample(random_recipe, 40)) #max = 80개씩 뿌릴수 있음
+        recommend_recipes = (random.sample(random_recipe, 40)) #max = 80개씩 뿌릴수 있음
 
-    return jsonify({'result': 'success', 'paik_random_recipe': paik_random_recipe})
+    return jsonify({'result': 'success', 'recommend_recipes': recommend_recipes})
 
 
 
